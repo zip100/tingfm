d45bcab0ba1dec51e64c282560d01a67c706474c
Mike.Guo <mike.guo@globalpay.com>
2023-07-07 17:01:05 +0800

    代码优化

--- main.go
+++ main.go
@@ -40,8 +40,16 @@ type ChannelResponse struct {
 
 var config Config
 
+const IndexPageUrl = "https://tingfm.com/region/cnr"
+const PlayListUrl = "https://tingfm.com/wp-json/query/wnd_posts?is_main_query=true&type=radio&without_content=true&paged=1&update_post_term_cache=false&update_post_meta_cache=false&posts_per_page=30&_term_region=3"
+const StreamUrl = "https://tingfm.com/wp-json/query/wndt_streams?post_id=4&in_web=true"
+
+/*
+* Parse Stream-Token from html
+* Init config
+ */
 func initConfig() {
-	resp, _ := http.Get("https://tingfm.com/region/cnr")
+	resp, _ := http.Get(IndexPageUrl)
 	body, _ := io.ReadAll(resp.Body)
 	bodyStr := string(body)
 	regex := regexp.MustCompile(`var wndt = (.*);`)
@@ -55,27 +63,47 @@ func main() {
 	fmt.Println(url)
 }
 
+/*
+* Fetch playlist
+ */
 func fetchPlayList() {
-	url := "https://tingfm.com/wp-json/query/wnd_posts?is_main_query=true&type=radio&without_content=true&paged=1&update_post_term_cache=false&update_post_meta_cache=false&posts_per_page=30&_term_region=3"
-	resp, _ := http.Get(url)
-	body, _ := io.ReadAll(resp.Body)
+	resp, err := http.Get(PlayListUrl)
+	if err != nil {
+		panic(err)
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
 	var resp1 ChannelListResponse
 	json.Unmarshal(body, &resp1)
 	fmt.Println(resp1)
 }
 
+/*
+* Fetch stream url
+ */
 func fetchChannelUrl() string {
-	url := "https://tingfm.com/wp-json/query/wndt_streams?post_id=4&in_web=true"
 	client := &http.Client{}
-	reqest, err := http.NewRequest("GET", url, nil)
+	reqest, err := http.NewRequest("GET", StreamUrl, nil)
 	reqest.Header.Add("Stream-Token", config.Token)
 	if err != nil {
 		panic(err)
 	}
-	response, _ := client.Do(reqest)
+
+	response, err := client.Do(reqest)
+	if err != nil {
+		panic(err)
+	}
 	defer response.Body.Close()
-	aa, _ := io.ReadAll(response.Body)
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
 	var channelResp ChannelResponse
-	json.Unmarshal(aa, &channelResp)
+	if nil != json.Unmarshal(body, &channelResp) {
+		panic("JSON Unmarshal error")
+	}
 	return channelResp.Data.Streams[0].Url
 }
